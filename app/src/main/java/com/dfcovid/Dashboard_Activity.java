8d0d6b7cd9048021f83dec042ae19fae9f971749
Admin <madhushree.kubsad@dfmail.org>
2021-05-26 20:38:04 +0530

    Madhu

--- app/src/main/java/com/dfcovid/Dashboard_Activity.java
+++ app/src/main/java/com/dfcovid/Dashboard_Activity.java
@@ -387,4 +387,13 @@ public class Dashboard_Activity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+
+    }
+
 }
