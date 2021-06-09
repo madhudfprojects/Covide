fa7fd5b192123b5643bd8377a74537e53ca8cd22
shivaleela <shivaleela.tech@dfmail.org>
2021-06-09 15:18:55 +0530

    5 files modified

--- app/src/main/java/com/dfcovid/Constants/Constants.java
+++ app/src/main/java/com/dfcovid/Constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
   //public static final String BASE_URL = "http://skilling.dfindia.org:8090/api/";
 //  public static final String BASE_URL = "https://skilling.dfindia.org:8090/api/";
   //public static final String BASE_URL = "https://covid.dfindia.org/Api/";
- public static final String BASE_URL = "http://ec2-3-1-63-28.ap-southeast-1.compute.amazonaws.com:8080/Api/";
+ //public static final String BASE_URL = "http://ec2-3-1-63-28.ap-southeast-1.compute.amazonaws.com:8080/Api/";
 
     //https://covid.dfindia.org/Api/Authentication/Get_UserHospitalList?User_Id=4
   //http://ec2-3-1-63-28.ap-southeast-1.compute.amazonaws.com:8080/Api/Authentication/Get_LoadHospitalData?Hospital_Id=28
@@ -17,5 +17,5 @@ public static final String CHANNEL_ID = "my_channel_01";
   //http://ec2-3-1-63-28.ap-southeast-1.compute.amazonaws.com:8080/Api/Authentication/Get_LoadHospitalData?Hospital_Id=2
 //  public static final String BASE_URL = "http://ec2-3-1-63-28.ap-southeast-1.compute.amazonaws.com:8080/Api/";
 
- // public static final String BASE_URL = "https://covid.dfindia.org/Api/";
+  public static final String BASE_URL = "https://covid.dfindia.org/Api/";
 }
